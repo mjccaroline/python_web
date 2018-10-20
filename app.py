95eac7ec1b5fa759786cc4d08f64c6bc43a73c18
mjccaroline <mjcaroline@gmail.com>
2018-10-20 15:46:44 +0530

    second

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from data import faculties
+
 app=Flask(__name__)
+
+getfaculties=faculties()
 
 @app.route('/')
 def index():
-    return 'hello'
+    return render_template('home.html')
 
 @app.route('/about')
 def abt():
     return 'about'
+
+@app.route('/send',methods=['GET','POST'])
+def send():
+    if(request.method=='POST'):
+        getname=request.form['name']
+        getemail=request.form['email']
+        return render_template('contact.html',name=getname,email=getemail)
+
+
+@app.route('/faculties')
+def faculties():
+    return render_template('faculties.html',sendfac=getfaculties)
 
 if(__name__=='__main__'):
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,14 @@
+def faculties():
+    facult=[
+        {"name":"ASD",
+        "Dept":"CSE"
+        },
+        {"name":"PQR",
+        "Dept":"EEE"
+        },
+        {"name":"QWE",
+        "Dept":"ECE"
+        }
+
+    ]
+    return facult
